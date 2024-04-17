7e18774eb106a3eb451d57586d4131b82233829b
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-04-17 13:00:00 +0330

    feat: Implement `Login` and `Logout` endpoint logic.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\AuthRequest;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -13,11 +14,39 @@ class AuthController extends Controller
         return view('auth.register');
     }
 
-    public function store(AuthRequest $request)
+    public function store(RegisterRequest $request)
     {
         $validated = $request->validated();
         User::create($validated);
 
         return redirect()->route('dashboard')->with('success', 'Account created successfully!');
+    }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate(LoginRequest $request)
+    {
+        $validated = $request->validated();
+        if (auth()->attempt($validated)) {
+            request()->session()->regenerate();
+
+            return redirect()->route('dashboard')->with('success', 'Account logged in successfully!');
+        }
+
+        return redirect()->route('login')->withErrors([
+            'password' => 'No matching user found with the provided email and password'
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
+        return redirect()->route('dashboard')->with('success', 'Account logged out successfully!');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,12 @@
-@extends('layouts.layout', ['title' => 'Register'])
+@extends('layouts.layout', ['title' => 'Login'])
 
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-12 col-sm-8 col-md-6">
-                <form class="form mt-5" action="{{ route('register.store') }}" method="post">
+                <form class="form mt-5" action="{{ route('login.authenticate') }}" method="post">
                     @csrf
-                    <h3 class="text-center text-dark">Register</h3>
-                    <div class="form-group">
-                        <label for="name" class="text-dark">Name:</label><br>
-                        <input type="text" name="name" id="name" class="form-control">
-                        @error('name')
-                        <span class="d-block fs-6 text-danger mt-2"> {{ $message }} </span>
-                        @enderror
-                    </div>
+                    <h3 class="text-center text-dark">Login</h3>
                     <div class="form-group mt-3">
                         <label for="email" class="text-dark">Email:</label><br>
                         <input type="email" name="email" id="email" class="form-control">
@@ -28,19 +21,12 @@
                         <span class="d-block fs-6 text-danger mt-2"> {{ $message }} </span>
                         @enderror
                     </div>
-                    <div class="form-group mt-3">
-                        <label for="password_confirmation" class="text-dark">Confirm Password:</label><br>
-                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
-                        @error('password_confirmation')
-                        <span class="d-block fs-6 text-danger mt-2"> {{ $message }} </span>
-                        @enderror
-                    </div>
                     <div class="form-group">
                         <label for="remember-me" class="text-dark"></label><br>
                         <input type="submit" name="submit" class="btn btn-dark btn-md" value="submit">
                     </div>
                     <div class="text-right mt-2">
-                        <a href="/login" class="text-dark">Login here</a>
+                        <a href="{{ route('register') }}" class="text-dark">Register here</a>
                     </div>
                 </form>
             </div>
