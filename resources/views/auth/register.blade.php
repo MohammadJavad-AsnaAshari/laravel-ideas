be10e177464894b49066010b266d62aa38daed2d
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-04-23 20:38:54 +0330

    feat: save `old inputs value` in `login` and `register` form.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,21 +9,21 @@
                     <h3 class="text-center text-dark">Register</h3>
                     <div class="form-group">
                         <label for="name" class="text-dark">Name:</label><br>
-                        <input type="text" name="name" id="name" class="form-control">
+                        <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
                         @error('name')
                         <span class="d-block fs-6 text-danger mt-2"> {{ $message }} </span>
                         @enderror
                     </div>
                     <div class="form-group mt-3">
                         <label for="email" class="text-dark">Email:</label><br>
-                        <input type="email" name="email" id="email" class="form-control">
+                        <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}">
                         @error('email')
                         <span class="d-block fs-6 text-danger mt-2"> {{ $message }} </span>
                         @enderror
                     </div>
                     <div class="form-group mt-3">
                         <label for="password" class="text-dark">Password:</label><br>
-                        <input type="password" name="password" id="password" class="form-control">
+                        <input type="password" name="password" id="password" class="form-control"}}">
                         @error('password')
                         <span class="d-block fs-6 text-danger mt-2"> {{ $message }} </span>
                         @enderror
