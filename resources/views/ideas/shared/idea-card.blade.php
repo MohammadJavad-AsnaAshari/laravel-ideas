2d525543e129e94223a4150e8e2c8ca0b9ffd331
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-05-29 17:53:56 +0330

    feat: Use `different data` for humans with `Carbon method` in idea and comments created_at.

--- resources/views/ideas/shared/idea-card.blade.php
+++ resources/views/ideas/shared/idea-card.blade.php
@@ -51,7 +51,7 @@
             @include('ideas.shared.like-button')
             <div>
                 <span class="fs-6 fw-light text-muted"> <span class="fas fa-clock"> </span>
-                    {{ $idea->created_at }} </span>
+                    {{ $idea->created_at->diffForHumans() }} </span>
             </div>
         </div>
         @include('shared.comments-box')
