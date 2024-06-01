8db8aba49bfa5776bc375556eea8a14b9cfd6422
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-06-01 12:26:20 +0330

    refactor: Remove unnecessary password cast in User Model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 
     public function ideas(): HasMany
