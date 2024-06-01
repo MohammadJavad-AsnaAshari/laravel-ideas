3a72ffdb5262b915cb77e6db2722fd085dd99f37
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-06-01 10:12:38 +0330

    fix: Redirect users from login and register if authenticated.

--- routes/auth.php
+++ routes/auth.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/register', [AuthController::class, 'store'])->name('register.store');
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'authenticate'])->name('login.authenticate');
-Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'store'])->name('register.store');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticate'])->name('login.authenticate');
+});
+
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
